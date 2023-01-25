e8f9d0057e002b7803dc694c3f12fe090eb8b6a3
myfreeer <myfreeer@users.noreply.github.com>
2023-01-25 16:16:14 +0800

    BMMultiColumn: switch upstream

--- chrome/BMMultiColumn.uc.js
+++ chrome/BMMultiColumn.uc.js
@@ -1,150 +1,201 @@
 // ==UserScript==
-// @name           BMMultiColumn.uc.js
-// @description    书签菜单自动分列显示（先上下后左右）
-// @author         ding
-// @include        main
-// @version        2019.12.23 适配 Firefox 72+
-// @version        2019.2.20 适配 Firefox 57+
+// @name            BMMultiColumn.uc.js
+// @description     书签菜单自动分列显示（先上下后左右）
+// @author          Ryan, ding
+// @include         main
+// @charset         UTF-8
+// @version         2022.12.22
+// @shutdown        window.BMMultiColumn.destroy();
+// @homepageURL     https://github.com/benzBrake/FirefoxCustomize/blob/master/userChromeJS
+// @notes           2022.12.22 融合 bookmarksmenu_scrollbar.uc.js，修复没超过最大宽度也会显示横向滚动条的 bug，支持主菜单的书签菜单
+// @note            2022.12.17 修复宽度异常，书签栏太多的话无法横向滚动，需要搭配 bookmarksmenu_scrollbar.uc.js 使用
+// @note            2022.11.19 fx 108 不完美修复
+// @note            2022.09.02 修复菜单延迟调整宽度的 BUG
+// @note            修复边距问题，支持书签工具栏溢出菜单
+// @note            修复有时候无法启用
+// @note            适配Firefox57+
+// @ignorecache
 // ==/UserScript==
+(function (css) {
+    const Services = globalThis.Services || Cu.import("resource://gre/modules/Services.jsm").Services;
+    const CustomizableUI = globalThis.CustomizableUI || Cu.import("resource:///modules/CustomizableUI.jsm").CustomizableUI;
 
-location.href.startsWith('chrome://browser/content/browser.x') && (function () {
+
     if (window.BMMultiColumn) {
         window.BMMultiColumn.destroy();
         delete window.BMMultiColumn;
     }
 
-    var BMMultiColumn = {
-        cachedMenus: [],
+    window.BMMultiColumn = {
+        menupopup: ["historyMenuPopup",
+            "bookmarksMenuPopup",
+            'PlacesToolbar',
+            'BMB_bookmarksPopup'],
+        timer: [],
+        count: [],
+        sss: Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService),
         init: function () {
-            $('PlacesToolbarItems').addEventListener('popupshowing', this, false);
-            var pop = $('BMB_bookmarksPopup');
-            if (pop) {
-                pop.addEventListener('popupshowing', this, false);
-                pop.addEventListener('click', this, false);
+            this.style = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
+            this.sss.loadAndRegisterSheet(this.style, 2);
+            window.removeEventListener("load", this, false);
+            window.addEventListener('unload', this, false);
+            window.addEventListener("aftercustomization", this, false);
+            this.delayedStartup();
+        },
+        delayedStartup: function () {
+            //wait till construction of bookmarksBarContent is completed.
+            for (var i = 0; i < this.menupopup.length; i++) {
+                this.count[i] = 0;
+                this.timer[i] = setInterval(function (self, i) {
+                    if (++self.count[i] > 50 || document.getElementById(self.menupopup[i])) {
+                        clearInterval(self.timer[i]);
+                        var menupopup = document.getElementById(self.menupopup[i]);
+                        if (menupopup) {
+                            menupopup.addEventListener('popupshowing', self, false);
+                        }
+                    }
+                }, 250, this, i);
             }
         },
-        destroy: function () {
-            $('PlacesToolbarItems').removeEventListener('popupshowing', this, false);
-            var pop = $('BMB_bookmarksPopup');
-            if (pop) {
-                pop.removeEventListener('popupshowing', this, false);
-                pop.removeEventListener('click', this, false);
-            }
-
-            var i;
-            for (i = 0; i < this.cachedMenus.length; i++) {
-                var menu = this.cachedMenus[i];
-                if (menu && menu._x_inited) {
-                    menu._x_scrollbox.width = '';
-                    if (menu._scrollBox && menu._scrollBox.style) {
-                        menu._scrollBox.style.maxHeight = "";
+        uninit: function () {
+            for (var i = 0; i < this.menupopup.length; i++) {
+                this.count[i] = 0;
+                this.timer[i] = setInterval(function (self, i) {
+                    if (++self.count[i] > 50 || document.getElementById(self.menupopup[i])) {
+                        clearInterval(self.timer[i]);
+                        var menupopup = document.getElementById(self.menupopup[i]);
+                        self.resetPopup(menupopup);
+                        if (menupopup) {
+                            menupopup.removeEventListener('popupshowing', self, false);
+                        }
                     }
-
-                    menu.style.maxWidth = "";
-
-                    var container = menu._x_box;
-                    if (container) {
-                        container.style.minHeight = "";
-                        container.style.height = "";
-                        container.style.display = "";
-                        container.style.flexFlow = "";
-                        container.style.overflow = "";
-                    }
-
-                    delete menu._x_scrollbox;
-                    delete menu._x_inited;
-                    delete menu._x_box;
-                }
+                }, 250, this, i);
             }
-            this.cachedMenus = [];
         },
-        handleEvent: function (event) {
-            var menupopup;
-            if (event.target.tagName === 'menu') {
-                menupopup = event.target.menupopup;
-            } else if (event.target.tagName === 'menupopup') {
-                menupopup = event.target;
-            } else return;
-            if (!menupopup) return;
-            //没有初始化或换过位置，重新设置属性
-            if (!menupopup.firstChild) return;
-
-            if (!menupopup._x_inited || !menupopup._x_scrollbox.scrollWidth) {
-                var scrollbox = menupopup._scrollBox ||
-                        menupopup.shadowRoot.querySelector('[part=arrowscrollbox]');
-
-                var firstMenu = menupopup.firstChild;
-                while (firstMenu) {
-                    if (firstMenu.tagName === "menuitem") break;
-                    firstMenu = firstMenu.nextSibling;
-                }
-
-                var box = firstMenu.parentElement._scrollBox.scrollbox ||
-                        scrollbox.shadowRoot.querySelector('[part=scrollbox]');
-
-                if (box) {
-                    menupopup._x_box = box;
-                    menupopup._x_scrollbox = scrollbox;
-                    if (!menupopup._x_inited) {
-                        menupopup._x_inited = true;
-                        this.cachedMenus.push(menupopup);
-                    }
-                }
-                // scrollbox.width = 'auto';
-                var container = menupopup._x_box;
-                if (container) {
-                    container.style.minHeight = "21px";
-                    container.style.height = "auto";
-                    container.style.display = "inline-flex";
-                    container.style.flexFlow = "column wrap";
-                    container.style.width = "auto";
-                    container.style.overflow = "-moz-hidden-unscrollable";
-                    menupopup._scrollBox.style.maxHeight = "calc(100vh - 129px)";
-                }
-                menupopup.style.maxWidth = "calc(100vw - 40px)";
+        destroy() {
+            window.removeEventListener('unload', this, false);
+            window.removeEventListener("aftercustomization", this, false);
+            this.uninit();
+            this.sss.unregisterSheet(this.style, 2);
+        },
+        handleEvent(event) {
+            switch (event.type) {
+                case 'popupshowing':
+                    let menupopup;
+                    if (event.target.tagName == 'menu' || event.target.tagName == 'toolbarbutton') {
+                        menupopup = event.target.menupopup;
+                    } else if (event.target.tagName == 'menupopup') {
+                        menupopup = event.target;
+                    } else return;
+                    this.initPopup(menupopup, event);
+                    break;
+                case 'aftercustomization':
+                    setTimeout(function (self) { self.delayedStartup(self); }, 0, this);
+                    break;
+                case 'unload':
+                    this.destroy();
+                    break;
             }
-            if (menupopup._x_inited) {
-                //如果菜单数量或者文字宽度发生变化，可能会多出空区域，重新计算下
-                var menuitem = menupopup.lastChild;
-                while (menuitem) {
-                    if (!menuitem.style.maxWidth) {
-                        menuitem.style.maxWidth = "300px";
-                        menuitem.style.minWidth = "100px";
-                    }
-                    menuitem = menuitem.previousSibling;
-                }
+        },
+        initPopup(menupopup, event) {
+            let arrowscrollbox = menupopup.shadowRoot.querySelector("::part(arrowscrollbox)");
+            let scrollbox = arrowscrollbox.shadowRoot.querySelector('[part=scrollbox]');
+            let inited = false;
+            if (scrollbox) {
+                inited = true;
+                scrollbox.style.minHeight = "21px";
+                scrollbox.style.height = "auto";
+                scrollbox.style.display = "flex";
+                scrollbox.style.flexFlow = "column wrap";
+                scrollbox.style.overflow = "-moz-hidden-unscrollable";
+                scrollbox.style.width = "unset";
+                arrowscrollbox.style.maxHeight = "calc(100vh - 129px)";
+            }
+            menupopup.style.maxWidth = "calc(100vw - 20px)";
 
-                var lastmenu = menupopup.lastChild;
+            if (inited) {
+                let maxWidth = parseInt(getComputedStyle(menupopup)['max-width']);
+                scrollbox.style.width = Math.min(maxWidth, scrollbox.scrollWidth) + "px";
+                if (maxWidth < scrollbox.scrollWidth) {
+                    scrollbox.style.setProperty("overflow-x", "auto", "important");
+                    scrollbox.style.setProperty("margin-top", "0", "important");
+                    scrollbox.style.setProperty("margin-bottom", "0", "important");
+                    // 上下のスクロールボタン
+                    event.originalTarget.on_DOMMenuItemActive = function (event) { };
+                    arrowscrollbox._scrollButtonUp.style.display = "none";
+                    arrowscrollbox._scrollButtonDown.style.display = "none";
+                }
+                let lastmenu = menupopup.lastChild;
                 while (lastmenu) {
                     if (lastmenu.scrollWidth >= 90) break;
                     lastmenu = lastmenu.previousSibling;
                 }
 
                 if (lastmenu && lastmenu.scrollWidth >= 90) {
-                    var pos1 = lastmenu.x - 0 + lastmenu.clientWidth;
-                    var pos2 = menupopup._x_box.x - 0 + menupopup._x_box.clientWidth;
+                    let pos1 = lastmenu.x - 0 + lastmenu.clientWidth;
+                    let pos2 = scrollbox.x - 0 + arrowscrollbox.clientWidth;
                     if (pos2 - pos1 > 30) {
-                        menupopup._x_scrollbox.width = "";
-                        menupopup._x_scrollbox.width = menupopup._x_box.scrollWidth;
-                    }
-                }
-                // 正确的计算顺序，先调整菜单宽度，再设置容器宽度
-                if (!(menupopup._x_scrollbox.clientWidth === menupopup._x_box.scrollWidth)) {
-                    menupopup._x_scrollbox.width = menupopup._x_box.scrollWidth;
-                }
-                //弹出菜单点击bug，要计算两次
-                if (event.type == "click") {
-                    if (!(menupopup._x_scrollbox.clientWidth === menupopup._x_box.scrollWidth)) {
-                        menupopup._x_scrollbox.width = menupopup._x_box.scrollWidth;
+                        arrowscrollbox.width = "";
+                        arrowscrollbox.width = scrollbox.scrollWidth;
                     }
                 }
             }
+        },
+        resetPopup(menupopup) {
+            let arrowscrollbox = menupopup.shadowRoot.querySelector("::part(arrowscrollbox)");
+            if (!arrowscrollbox) return;
+            arrowscrollbox.style.maxHeight = "";
+            let scrollbox = arrowscrollbox.shadowRoot.querySelector('[part=scrollbox]');
+
+            if (!scrollbox) return;
+            scrollbox.style.minHeight = "";
+            scrollbox.style.height = "";
+            scrollbox.style.display = "";
+            scrollbox.style.flexFlow = "";
+            scrollbox.style.overflow = "";
+            scrollbox.style.maxHeight = "";
+            scrollbox.style.width = "";
+
+            let menuitem = menupopup.lastChild;
+            while (menuitem) {
+                menuitem.style.width = "";
+                menuitem = menuitem.previousSibling;
+            }
         }
     }
-    BMMultiColumn.init();
-    window.BMMultiColumn = BMMultiColumn;
 
-    function $(id) {
-        return document.getElementById(id);
+
+    function $(id, aDoc) {
+        id = id || "";
+        let doc = aDoc || document;
+        if (id.startsWith('#')) id = id.substring(1, id.length);
+        return doc.getElementById(id);
     }
-})();
+
+    if (typeof _ucUtils !== 'undefined') {
+        _ucUtils.startupFinished()
+            .then(() => {
+                window.BMMultiColumn.init();
+            });
+    } else {
+        if (gBrowserInit.delayedStartupFinished) window.BMMultiColumn.init();
+        else {
+            let delayedListener = (subject, topic) => {
+                if (topic == "browser-delayed-startup-finished" && subject == window) {
+                    Services.obs.removeObserver(delayedListener, topic);
+                    window.BMMultiColumn.init();
+                }
+            };
+            Services.obs.addObserver(delayedListener, "browser-delayed-startup-finished");
+        }
+    }
+})(`
+#PlacesToolbarItems menupopup {
+    max-width: calc(100vw - 20px);
+    max-height: calc(100vh - 129px);
+  }
+#PlacesToolbarItems menu.bookmark-item, #PlacesToolbarItems menuitem.bookmark-item {
+  min-width: 0;
+  max-width: 26em;
+}
+`)
